bce9a39ba54b51a121b7735b9e4cc7d18640491b
Naman Rajpal <namanrajpal16@gmail.com>
2015-09-25 15:02:12 +0530

    VRProject
    >A project based on Virtual Reality.
    >We are developing a game which will be able to run on any Head Mounted
    device based on the concept of Google Cardboard.
    >We are using Unity3D 4.6.1 to develop our virtually real environment.
    >As per the project requirements, we need to to add networking
    capabilities to the game. Multiple people present in the same VR
    environment should be able to interact in any of the possible ways.
    >For networking, Instead of writing native socket code and to prevent us
    from the tedious job of network backend coding(which is not our motive)
    , we will be using "Nextpeer SDK" for Unity which provide enhanced
    social and networking features for the game.

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z+speed*Time.deltaTime);
+	
+	}
+}
---
+++ Assets/FrontSpawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class FrontSpawner : MonoBehaviour {
+
+
+	public GameObject ground;
+	Vector3 spawnpos;
+
+	// Use this for initialization
+	void Start () {
+		spawnpos = new Vector3 (0f, 0f, 60f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+
+		Instantiate (ground, transform.position+spawnpos, Quaternion.identity);
+
+		Destroy (gameObject);
+
+	}
+}
